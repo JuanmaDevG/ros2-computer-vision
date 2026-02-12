4d615a26abd9873b2f1892de83acd102057b5c10
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-02-12 21:49:12 +0100

    Accidentally uploaded build files and so removed them

--- ros2_ws/wander/build/wander/CMakeFiles/wander_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/wander_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/wander_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
